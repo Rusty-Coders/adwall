19a756a91d8d976c8cebeec47427bc11eb06a908
SwapnilShahGit <swapnilshahmail@gmail.com>
2018-12-27 18:06:43 -0800

    passing onto rajdeep

---
+++ src/parse.rs
@@ -0,0 +1,42 @@
+
+fn parseWebpage() {
+//
+}
+
+fn parseFilterLists() {
+// read through lists and store in memory into respective data structures
+}
+
+fn isBlocked() {
+// Search through isBlocked Data Structure to see if theres a hit
+}
+
+fn isException() {
+//
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn it_doesnt_work() {
+        assert_ne!(2 + 2, 5);
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!();
+    }
+
+    fn testIsBlocked() {
+
+    }
+    fn testIsException() {
+
+    }
+}
